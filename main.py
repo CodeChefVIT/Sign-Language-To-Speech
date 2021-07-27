49cabc90ff4aa9ecb820d3e611ea288cdfbdf760
VanshikaNehra23 <vanshika.nehra23@gmail.com>
2021-07-27 12:09:06 +0530

    Modified Flask HTML CSS

---
+++ main.py
@@ -0,0 +1,27 @@
+import pyttsx3
+
+
+def text_to_speech(text, gender):
+    """
+    Function to convert text to speech
+    :param text: text
+    :param gender: gender
+    :return: None
+    """
+    voice_dict = {'Male': 0, 'Female': 1}
+    code = voice_dict[gender]
+
+    engine = pyttsx3.init()
+
+    # Setting up voice rate
+    engine.setProperty('rate', 125)
+
+    # Setting up volume level  between 0 and 1
+    engine.setProperty('volume', 0.8)
+
+    # Change voices: 0 for male and 1 for female
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[code].id)
+
+    engine.say(text)
+    engine.runAndWait()
